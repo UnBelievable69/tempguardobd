5a804139a552712ca118d29d78af793df61609cb
UnBelievable69 <36425442+UnBelievable69@users.noreply.github.com>
2026-08-02 01:30:12 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,14 +5,6 @@ let package = Package(
     name: "FanController",
     platforms: [.iOS(.v15)],
     products: [.executable(name: "FanController", targets: ["FanController"])],
-    dependencies: [
-        .package(url: "https://github.com", from: "1.0.0")
-    ],
-    targets: [
-        .executableTarget(
-            name: "FanController",
-            dependencies: ["SwiftOBD2"],
-            path: "Sources"
-        )
-    ]
+    dependencies: [.package(url: "https://github.com", from: "1.0.0")],
+    targets: [.executableTarget(name: "FanController", dependencies: ["SwiftOBD2"], path: "Sources")]
 )
